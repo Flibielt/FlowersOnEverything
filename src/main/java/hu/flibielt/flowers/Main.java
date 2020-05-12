0839bf77cac3f6c227bef10adc53b711b26e1b25
Csaba Hermann <hcsabi98@gmail.com>
2020-05-12 12:50:30 +0200

    Created Main.java
    The main entry of the plugin

---
+++ src/main/java/hu/flibielt/flowers/Main.java
@@ -0,0 +1,12 @@
+package hu.flibielt.flowers;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        System.out.println("FLOWERS ON EVERYTHING IS ENABLED!");
+    }
+}
